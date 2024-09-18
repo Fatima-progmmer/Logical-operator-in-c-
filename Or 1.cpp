679b7b2e020f5d9b33766b52258f32b9abb03b2a
Tanzeela Fatima <149133736+Fatima-progmmer@users.noreply.github.com>
2024-09-18 14:48:06 +0500

    Add files via upload

---
+++ Or 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	do{
+		cout << "Enter a number :";
+		cin >> n;
+	}
+	while(n!=0);
+	cout << "Hello" << endl;
+	return 0;
+}
+
---
+++ and 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << "Enter a number :";
+	cin >> a;
+	int b;
+	cout << "Enter a number :";
+	cin >> b;
+	while(b>0&&a>0)
+	cout << " You entered Positive number that are greater than 0" << endl;
+	return 0;
+}
+
